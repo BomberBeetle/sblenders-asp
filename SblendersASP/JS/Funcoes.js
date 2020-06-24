2b9a93a625d83fc1d59a0fd159332b9478e1886c
RAFAEL DE OLIVEIRA MAGALHAES <rafaelom04@gmail.com>
2020-06-24 15:56:03 -0300

    Pequenas Mudanças nos produtos

--- SblendersASP/JS/Funcoes.js
+++ SblendersASP/JS/Funcoes.js
@@ -100,7 +100,17 @@ function fecharMusica() {
             set: (classe) => {
             this.className = classe; (indexDinamico());
 }}})
-}*/
+}
+
+var visibilidadeBurguer1 = true;
+var visibilidadeBurguer2 = false;
+var visibilidadeBurguer3 = false;
+var visibilidadeBurguer4 = false;
+var visibilidadeBurguer5 = false;
+var visibilidadeBurguer6 = false;
+var visibilidadeBurguer7 = false;
+var visibilidadeBurguer8 = false;
+var visibilidadeBurguer9 = false;
 
 function AdicionarLanche() {
     var divBurguer1 = document.getElementById("divBurguer1");
@@ -111,11 +121,40 @@ function AdicionarLanche() {
     var divBurguer6 = document.getElementById("divBurguer6");
     var divBurguer7 = document.getElementById("divBurguer7");
     var divBurguer8 = document.getElementById("divBurguer8");
-    var divBurguer9 = document.getElementById("divBurguer9");
-    
+    var divBurguer9 = document.getElementById("divBurguer9");   
 
-    if (divBurguer2.style.display = "none") {
-        alert("sggsdgsg");
+    if (visibilidadeBurguer2 === false) {
         divBurguer2.style.display = "block";
+        visibilidadeBurguer2 = true;
     }
-}
+    else if (visibilidadeBurguer3 === false) {
+        divBurguer3.style.display = "block";
+        visibilidadeBurguer3 = true;
+    }
+    else if (visibilidadeBurguer4 === false) {
+        divBurguer4.style.display = "block";
+        visibilidadeBurguer4 = true;
+    }
+    else if (visibilidadeBurguer5 === false) {
+        divBurguer5.style.display = "block";
+        visibilidadeBurguer5 = true;
+    }
+    else if (visibilidadeBurguer6 === false) {
+        divBurguer6.style.display = "block";
+        visibilidadeBurguer6 = true;
+    }
+    else if (visibilidadeBurguer7 === false) {
+        divBurguer7.style.display = "block";
+        visibilidadeBurguer7 = true;
+    }
+    else if (visibilidadeBurguer8 === false) {
+        divBurguer8.style.display = "block";
+        visibilidadeBurguer8 = true;
+    }
+    else if (visibilidadeBurguer9 === false) {
+        divBurguer9.style.display = "block";
+        visibilidadeBurguer9 = true;
+    }
+
+    event.preventDefault();
+}*/
